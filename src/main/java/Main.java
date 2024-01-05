0f2156de268b5523ae74923fe847caf44ab6c505
devNagy <Nagy.Devon@student.greenriver.edu>
2024-01-05 14:38:58 -0800

    Re-added the printMessage() method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
-        System.out.println("By: Josh");
+        printMessage("Hello, world");
+        printMessage("By: Devon");
+    }
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 }
