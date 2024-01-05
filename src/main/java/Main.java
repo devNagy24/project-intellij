1c7f0d88d9d739dbb14c26e326a419aa7d5643f1
devNagy <Nagy.Devon@student.greenriver.edu>
2024-01-05 14:37:20 -0800

    Revert "Refactored my Main class."
    This reverts commit b6631870

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        printHeader();
-        printMessage("Hello, world");
-
-        printHeader();
-        printMessage("By: Devon");
-    }
-    private static void printMessage(String message) {
-        System.out.println(message);
-    }
-
-    private static void printHeader() {
-        System.out.println("*************************************");
+        System.out.println("Hello, world");
+        System.out.println("By: Josh");
     }
 }
